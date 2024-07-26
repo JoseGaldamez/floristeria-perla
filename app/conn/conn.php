aefda7ba69dc469f330267e46ccc40f5e5fe472e
José Galdámez <josegaldamez1991@gmail.com>
2024-07-25 18:22:16 -0600

    connexion to db

---
+++ app/conn/conn.php
@@ -0,0 +1,10 @@
+<?php
+
+include 'env_vars.php';
+
+$conn = mysqli_connect($serverName, $userName, $password, $database);
+if (!$conn) {
+    die("Error al conectarse a la base de datos." . mysqli_connect_error());
+}
+
+echo "Conexión exitosa";
---
+++ profile/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Floristería Perla</title>
+</head>
+
+<body>
+
+    <h1>Profile</h1>
+
+    <?php
+    include "../app/conn/conn.php";
+    ?>
+
+</body>
+
+</html>
