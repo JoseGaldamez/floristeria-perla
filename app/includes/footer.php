60faecdd262806e6d8fe5b97debf2d22a0113674
Christian Vasquez <VasqCD@users.noreply.github.com>
2024-07-27 23:44:01 -0600

    chore: Add footer section to landing page

---
+++ app/includes/footer.php
@@ -0,0 +1,49 @@
+<footer>
+    <div class="container-fluid bg-pink-primary text-white p-5">
+        <div class="row">
+            <div class="col-xs-12 col-md-6 col-lg-3">
+                <h3>Contáctanos</h3>
+                <p>Envíanos un mensaje a nuestro correo electrónico: <a href="mailto:contacto@flores.com" class="text-white">contacto@flores.com</a></p>
+                <p>Visítanos en nuestras redes sociales:</p>
+                <div class="d-flex flex-column flex-md-row">
+                    <a href="https://www.facebook.com" target="_blank" class="d-flex align-items-center text-decoration-none text-white me-3 mb-2 mb-md-0">
+                        <i class="bi bi-facebook fs-3"></i>
+                        <span class="ms-2">Facebook</span>
+                    </a>
+                    <a href="https://www.instagram.com" target="_blank" class="d-flex align-items-center text-decoration-none text-white me-3 mb-2 mb-md-0">
+                        <i class="bi bi-instagram fs-3"></i>
+                        <span class="ms-2">Instagram</span>
+                    </a>
+                    <a href="https://www.tiktok.com" target="_blank" class="d-flex align-items-center text-decoration-none text-white mb-2 mb-md-0">
+                        <i class="bi bi-tiktok fs-3"></i>
+                        <span class="ms-2">TikTok</span>
+                    </a>
+                </div>
+            </div>
+            <div class="col-xs-12 col-md-6 col-lg-3">
+                <h3>Información</h3>
+                <ul>
+                    <li><a href="#" class="text-white">Sobre Nosotros</a></li>
+                    <li><a href="#" class="text-white">Términos y Condiciones</a></li>
+                    <li><a href="#" class="text-white">Política de Privacidad</a></li>
+                    <li><a href="#" class="text-white">Preguntas Frecuentes</a></li>
+                </ul>
+            </div>
+            <div class="col-xs-12 col-md-6 col-lg-3">
+                <h3>Explora productos</h3>
+                <ul>
+                    <li><a href="#" class="text-white">Ramos de Rosas</a></li>
+                    <li><a href="#" class="text-white">Arreglos Mixtos</a></li>
+                    <li><a href="#" class="text-white">Cajas de Rosas</a></li>
+                    <li><a href="#" class="text-white">Flores Variadas</a></li>
+                </ul>
+            </div>
+            <div class="col-xs-12 col-md-6 col-lg-3">
+                <h3>Horario de Atención</h3>
+                <p>Lunes a Viernes: 8:00 am - 6:00 pm</p>
+                <p>Sábados: 8:00 am - 12:00 pm</p>
+                <p>Domingos: Cerrado</p>
+            </div>
+        </div>
+    </div>
+</footer>
