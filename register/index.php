a8642fd62d4216f9dbcf2717c7a4a12f142a11ea
perlapuerto <perlapuerto8@gmail.com>
2024-07-28 20:21:49 -0600

    Agregado de Registros
    Agregue la maquetación de Registro el fondo de la pagina de Registro va dentro de una carpeta de assets

--- register/index.php
+++ register/index.php
@@ -5,14 +5,45 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Floristería Perla</title>
+    
 </head>
 
 <body>
 
-    <h1>Login</h1>
+    <h1>Registro</h1>
 
     <?php
     include "../app/conn/conn.php";
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6 register-container">
+                <h2 class="text-center mt-3">Registro de Usuario</h2>
+                <form action="register.php" method="post">
+                    <div class="form-group">
+                        <label for="username">Nombre de usuario</label>
+                        <input type="text" class="form-control" id="username" name="username" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Correo electrónico</label>
+                        <input type="email" class="form-control" id="email" name="email" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Contraseña</label>
+                        <input type="password" class="form-control" id="password" name="password" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="confirm_password">Confirmar contraseña</label>
+                        <input type="password" class="form-control" id="confirm_password" name="confirm_password" required>
+                    </div>
+                    <button type="submit" class="btn btn-pink btn-block">Registrarse</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     ?>
 
 </body>
