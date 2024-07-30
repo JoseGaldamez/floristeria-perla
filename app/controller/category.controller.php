90634e34e2d31d32076e87d99478e5990b1b30ab
José Galdámez <josegaldamez1991@gmail.com>
2024-07-30 14:33:52 -0600

    validando post

--- app/controller/category.controller.php
+++ app/controller/category.controller.php
@@ -2,13 +2,15 @@
 header('Content-type: application/json');
 include_once '../model/category.model.php';
 
-if (isset($_POST['categoryName']) && $_POST['categoryName'] && isset($_POST['description']) && $_POST['description']) {
+if ($_POST) {
+    if (isset($_POST['categoryName']) && $_POST['categoryName'] && isset($_POST['description']) && $_POST['description']) {
 
-    if (saveNewCategory($_POST['categoryName'], $_POST['description'])) {
-        echo json_encode(array('success' => true, 'message' => "Categoría creada correctamente"));
+        if (saveNewCategory($_POST['categoryName'], $_POST['description'])) {
+            echo json_encode(array('success' => true, 'message' => "Categoría creada correctamente"));
+        } else {
+            echo json_encode(array('success' => false, 'message' => "Algo falló creando la categoría"));
+        }
     } else {
-        echo json_encode(array('success' => false, 'message' => "Algo falló creando la categoría"));
+        echo json_encode(array('success' => false, 'message' => "Parámetros faltantes."));
     }
-} else {
-    echo json_encode(array('success' => false, 'message' => "Parámetros faltantes."));
 }
